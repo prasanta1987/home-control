689fd28b79bc2ceb1000f0c2fadff5b92c28d95b
prasanta1987 <prasanta.1987@hotmail.com>
2018-09-30 19:06:33 +0530

    changes

--- public/js/control.js
+++ public/js/control.js
@@ -1,12 +1,12 @@
 
-// Initialize Firebase
+// Initialize Firebase with your credentials
 // var config = {
-//     apiKey: "AIzaSyAm6oCZsb_pAtygV6eGsJJk51Dv1ou6JL4",
-//     authDomain: "pk-home.firebaseapp.com",
-//     databaseURL: "https://pk-home.firebaseio.com",
-//     projectId: "pk-home",
-//     storageBucket: "pk-home.appspot.com",
-//     messagingSenderId: "421147871607"
+//     apiKey: "",
+//     authDomain: "",
+//     databaseURL: "",
+//     projectId: "",
+//     storageBucket: "",
+//     messagingSenderId: ""
 // };
 // firebase.initializeApp(config);
 
